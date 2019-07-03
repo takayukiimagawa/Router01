39f03d763d007028e78cbe767643f0de1cca915c
Takayuki Imagawa <imapaso16@gmail.com>
2019-07-03 16:40:54 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Link } from "react-router-dom";
+import About from "./components/about";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Link to="/about">About</Link>
+      <Route path="/about" component={About} />
     </div>
   );
 }
